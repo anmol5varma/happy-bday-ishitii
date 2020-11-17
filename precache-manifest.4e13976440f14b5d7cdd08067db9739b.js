7520a7c0d679a2bdda288a544bf99270b7d94b8f
Anmol Varma <anmol_varma@mckinsey.com>
2020-11-17 15:20:38 +0530

    Updates

---
+++ precache-manifest.4e13976440f14b5d7cdd08067db9739b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "adf1951466e0cdc67130",
+    "url": "/happy-bday-ishitii/static/css/main.7b03e0fa.chunk.css"
+  },
+  {
+    "revision": "adf1951466e0cdc67130",
+    "url": "/happy-bday-ishitii/static/js/main.adf19514.chunk.js"
+  },
+  {
+    "revision": "4ad756683673bcb3be35",
+    "url": "/happy-bday-ishitii/static/js/1.4ad75668.chunk.js"
+  },
+  {
+    "revision": "9f26cda3e25857790573",
+    "url": "/happy-bday-ishitii/static/js/runtime~main.9f26cda3.js"
+  },
+  {
+    "revision": "2b0b6409d13118be48cf05be6a02ad2b",
+    "url": "/happy-bday-ishitii/static/media/confetti.2b0b6409.gif"
+  },
+  {
+    "revision": "fd47ed0621d942e3dd147adba4a308b1",
+    "url": "/happy-bday-ishitii/static/media/anmol.fd47ed06.jpeg"
+  },
+  {
+    "revision": "fce1a709aee9b2641b2bde91758ba1f0",
+    "url": "/happy-bday-ishitii/static/media/anmol_2.fce1a709.jpeg"
+  },
+  {
+    "revision": "02ace0cf0b578b3f7ee80e0a8e5d452c",
+    "url": "/happy-bday-ishitii/static/media/aparna.02ace0cf.jpeg"
+  },
+  {
+    "revision": "163b1f38bfb275a93da053ef8e42a7f7",
+    "url": "/happy-bday-ishitii/static/media/aparna_1.163b1f38.jpeg"
+  },
+  {
+    "revision": "fd86195a442daf73ad958e5bdb707322",
+    "url": "/happy-bday-ishitii/static/media/apu.fd86195a.jpeg"
+  },
+  {
+    "revision": "9bc57b0759a3f31853a7181827ecf0ce",
+    "url": "/happy-bday-ishitii/static/media/budda.9bc57b07.jpeg"
+  },
+  {
+    "revision": "ee045ee3ff31415dc136336696a29a32",
+    "url": "/happy-bday-ishitii/static/media/paaji_1.ee045ee3.jpeg"
+  },
+  {
+    "revision": "3b73014420d3d36aabdcf2d41fec83f5",
+    "url": "/happy-bday-ishitii/static/media/paaji_2.3b730144.jpeg"
+  },
+  {
+    "revision": "a8db7e92ee6fe5adf863b12bacb33105",
+    "url": "/happy-bday-ishitii/static/media/pari.a8db7e92.jpg"
+  },
+  {
+    "revision": "cbcfe2adf622ec1d83c35d7fb8f2185a",
+    "url": "/happy-bday-ishitii/static/media/prabodh.cbcfe2ad.jpeg"
+  },
+  {
+    "revision": "3525dc3d68a1ff20ec9079df1a7fbb3a",
+    "url": "/happy-bday-ishitii/static/media/priyanshi.3525dc3d.jpeg"
+  },
+  {
+    "revision": "f6f714182116ff3686d29657f889a56d",
+    "url": "/happy-bday-ishitii/static/media/shubhanshu.f6f71418.jpeg"
+  },
+  {
+    "revision": "8c1915942a7b4f1c55b102a4e513102a",
+    "url": "/happy-bday-ishitii/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/happy-bday-ishitii/precache-manifest.3765259849663d57d6da97ce6dbd2442.js"
+  "/happy-bday-ishitii/precache-manifest.4e13976440f14b5d7cdd08067db9739b.js"
 );
 
 workbox.clientsClaim();
