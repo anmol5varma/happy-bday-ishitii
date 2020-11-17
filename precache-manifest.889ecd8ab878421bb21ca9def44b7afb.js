64bf2492c840b19cb371e4faab51f117504ca958
Anmol Varma <anmol_varma@mckinsey.com>
2020-11-17 10:18:58 +0530

    Updates

---
+++ precache-manifest.889ecd8ab878421bb21ca9def44b7afb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cc4849ed1bb9aad6cac",
+    "url": "/happy-bday-ishitii/static/css/main.7b03e0fa.chunk.css"
+  },
+  {
+    "revision": "9cc4849ed1bb9aad6cac",
+    "url": "/happy-bday-ishitii/static/js/main.9cc4849e.chunk.js"
+  },
+  {
+    "revision": "4ad756683673bcb3be35",
+    "url": "/happy-bday-ishitii/static/js/1.4ad75668.chunk.js"
+  },
+  {
+    "revision": "9f26cda3e25857790573",
+    "url": "/happy-bday-ishitii/static/js/runtime~main.9f26cda3.js"
+  },
+  {
+    "revision": "2b0b6409d13118be48cf05be6a02ad2b",
+    "url": "/happy-bday-ishitii/static/media/confetti.2b0b6409.gif"
+  },
+  {
+    "revision": "fd47ed0621d942e3dd147adba4a308b1",
+    "url": "/happy-bday-ishitii/static/media/anmol.fd47ed06.jpeg"
+  },
+  {
+    "revision": "fce1a709aee9b2641b2bde91758ba1f0",
+    "url": "/happy-bday-ishitii/static/media/anmol_2.fce1a709.jpeg"
+  },
+  {
+    "revision": "9bc57b0759a3f31853a7181827ecf0ce",
+    "url": "/happy-bday-ishitii/static/media/budda.9bc57b07.jpeg"
+  },
+  {
+    "revision": "8e738dad927d07785c03ffe62c1ab67a",
+    "url": "/happy-bday-ishitii/static/media/prabodh.8e738dad.jpeg"
+  },
+  {
+    "revision": "09bda142364b28de6e50183880ab5a93",
+    "url": "/happy-bday-ishitii/index.html"
+  }
+];
